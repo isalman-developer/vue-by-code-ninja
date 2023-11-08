febab4ebced00480fe2225f8c537d65c5fa42f6f
dev-salman <isalman.developer@gmail.com>
2023-11-08 12:40:30 +0500

    forward back buttons

--- src/views/jobs/Jobs.vue
+++ src/views/jobs/Jobs.vue
@@ -1,28 +1,37 @@
 <template>
-    <RouterHistoryButton/>
+    <RouterHistoryButton />
     <h1>Jobs</h1>
-    <div v-for="job in jobs" :key="job.id">
-        <router-link :to="{ name: 'job-details', params: { id: job.id } }">
-            <h2>{{ job.title }}</h2> 
-        </router-link>
+    <div v-if="jobs">
+        <div v-for="job in jobs" :key="job.id">
+            <router-link :to="{ name: 'job-details', params: { id: job.id } }">
+                <h2>{{ job.title }}</h2>
+            </router-link>
+        </div>
+    </div>
+    <div v-else>
+        <p>
+            Fetching Records, if record not found. Run json-serve --watch data/db.json
+            or Check data in data/db.jsonx
+        </p>
     </div>
 </template>
 <script>
-import RouterHistoryButton from './RouterHistoryButton.vue';
+import RouterHistoryButton from "./RouterHistoryButton.vue";
 export default {
-    components : {RouterHistoryButton},
+    components: { RouterHistoryButton },
     data() {
         return {
-            jobs: []
-        }
+            jobs: [],
+        };
     },
-    
+
     mounted() {
-        fetch('http://localhost:3000/blogs')
-        .then(function(res){ //short syntax is .then(res => res.json())
-            return res.json();
-        }).then(data => this.jobs = data)
+        fetch("http://localhost:3000/blogs")
+            .then(function (res) {
+                //short syntax is .then(res => res.json())
+                return res.json();
+            })
+            .then((data) => (this.jobs = data));
     },
-    
-}
+};
 </script>
